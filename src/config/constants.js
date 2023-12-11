8881bba87def9f6425b3b526044717a644a7bd07
ludozing <luke9210@inno-datasys.com>
2023-12-11 10:33:17 +0900

    서버 주소 원복

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,8 @@
-// export const API_URL = "https://esv.redmetas.com";
-// export const STORAGE_URL = "https://esv.redmetas.com/static";
+export const API_URL = "https://esv.redmetas.com";
+export const STORAGE_URL = "https://esv.redmetas.com/static";
 // export const API_URL = "http://localhost:8999";
 // export const STORAGE_URL = "http://localhost:8999/static";
 
 
-export const API_URL = "http://223.130.130.219:8008";
-export const STORAGE_URL = "http://223.130.130.219:8008/static";
+// export const API_URL = "http://223.130.130.219:8008";
+// export const STORAGE_URL = "http://223.130.130.219:8008/static";
